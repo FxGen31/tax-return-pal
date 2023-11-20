4d53514929b1d845452f94d62eeb19527efb3757
FxGen31 <fxgen31@gmail.com>
2023-11-21 10:30:29 +1100

    feat: add dialog to add new repository

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use crate::lib::set_window_shadow;
-
 mod lib;
+
+use crate::lib::set_window_shadow;
 
 fn main() {
     tauri::Builder::default()
